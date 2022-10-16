2f672dfc1988b20a26edbc6ad1b3ad894d80e5dc
ankit1250 <ak656634@gmail.com>
2022-10-16 15:46:18 +0530

    first eclipse commit

---
+++ Demo/src/org/easeyourstudy/DebuggingPractice.java
@@ -0,0 +1,13 @@
+package org.easeyourstudy;
+
+public class DebuggingPractice {
+
+	public static void main(String[] args) {
+		int x =5;
+		int y = 0;
+		int z= x/y;
+		System.out.println(z);
+
+	}
+
+}
